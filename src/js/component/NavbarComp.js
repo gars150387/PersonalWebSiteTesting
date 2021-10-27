6e59f1ecb80722db747191d7963cb2688562a8f6
gars150387 <gar.santeliz@gmail.com>
2021-10-27 21:55:58 +0000

    new files

---
+++ src/js/component/NavbarComp.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+export const NavbarComp = () => {
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="#home">
+					<Link to="/">Gustavo Rodriguez</Link>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+						<Link to="/aboutMe">
+							<Nav.Link href="#aboutMe">About Me</Nav.Link>
+						</Link>
+						<Link to="/projects">
+							<Nav.Link href="#projects">Projects</Nav.Link>
+						</Link>
+						<NavDropdown title="GitHub" id="collasible-nav-dropdown">
+							<NavDropdown.Item href="https://github.com/gars150387/Contact_list_Flux_API_BasedOnArticle">
+								Contact List
+							</NavDropdown.Item>
+							<NavDropdown.Item href="https://github.com/gars150387/todo-list-in-landing-page-react-flux">
+								To-Do List
+							</NavDropdown.Item>
+							<NavDropdown.Item href="https://github.com/gars150387/simple-counter-exercise-react-hook">
+								Simple Counter Exercise
+							</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="https://github.com/gars150387/random-excuse-exercise-javascript">
+								Random Excuse Exercise
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+					<Nav>
+						<Link to="/certifications">
+							<Nav.Link href="#certifications">Certifications</Nav.Link>
+						</Link>
+						<Link to="/contact">
+							<Nav.Link href="#contact">Contact</Nav.Link>
+						</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
---
+++ src/js/views/AboutMe.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Row, Col, Container, Image } from "react-bootstrap";
+
+export const AboutMe = () => {
+	return (
+		<>
+			<Container>
+				<Row>
+					<Col xs={6} md={4}>
+						<Image className="justify-content-end" src="gustavo-san-francisco.jpg" rounded />
+					</Col>
+				</Row>
+			</Container>
+			<p>Hello World this is me!</p>
+		</>
+	);
+};
