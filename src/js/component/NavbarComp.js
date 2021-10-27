6b7745a0ef74ea055f966cfe1a2845e777bc4dc3
gars150387 <gar.santeliz@gmail.com>
2021-10-27 23:42:58 +0000

    adding more href to Github option on navbar

--- src/js/component/NavbarComp.js
+++ src/js/component/NavbarComp.js
@@ -12,7 +12,7 @@ export const NavbarComp = () => {
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="me-auto">
+					<Nav className="me-auto justify-content-end">
 						<Link to="/aboutMe">
 							<Nav.Link href="#aboutMe">About Me</Nav.Link>
 						</Link>
@@ -28,6 +28,9 @@ export const NavbarComp = () => {
 							</Link>
 						</Nav>
 						<NavDropdown title="GitHub" id="collasible-nav-dropdown">
+							<NavDropdown.Item href="https://github.com/WoodlyB/Gustavo-and-Woodly-project/tree/main/src/js/views">
+								Deyo Project
+							</NavDropdown.Item>
 							<NavDropdown.Item href="https://github.com/gars150387/Contact_list_Flux_API_BasedOnArticle">
 								Contact List
 							</NavDropdown.Item>
@@ -37,9 +40,15 @@ export const NavbarComp = () => {
 							<NavDropdown.Item href="https://github.com/gars150387/simple-counter-exercise-react-hook">
 								Simple Counter Exercise
 							</NavDropdown.Item>
-							<NavDropdown.Divider />
+							{/* <NavDropdown.Divider /> */}
 							<NavDropdown.Item href="https://github.com/gars150387/random-excuse-exercise-javascript">
 								Random Excuse Exercise
+							</NavDropdown.Item>
+							<NavDropdown.Item href="https://github.com/gars150387/traffic-light-exercise-react">
+								Traffic Light Exercise
+							</NavDropdown.Item>
+							<NavDropdown.Item href="https://github.com/gars150387/Appointment_App">
+								Appointment App from LinkedIn Learning
 							</NavDropdown.Item>
 						</NavDropdown>
 					</Nav>
