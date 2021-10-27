53f4c4eb16d4cc8af0f4c8a061e43aef1bcebc73
gars150387 <gar.santeliz@gmail.com>
2021-10-27 23:00:29 +0000

    fixing props to map data

--- src/js/component/CardComp.js
+++ src/js/component/CardComp.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { DataCertifications } from "./DataCertifications";
 
-export const CardComp = ({ title, company, img, link }) => {
+export const CardComp = ({ title, company, link }) => {
 	return (
 		<>
-			<Card style={{ width: "18rem" }} key={id}>
-				<Card.Img variant="top" src={`${img}`} />
-				<Card.Body>
-					<Card.Title>{title}</Card.Title>
-					<Card.Text>{company}</Card.Text>
-					<Button variant="primary" href={`${link}`}>
-						Go and Check it out
-					</Button>
-				</Card.Body>
-			</Card>
+			{DataCertifications.map(index => {
+				return (
+					<li key={index}>
+						<Card style={{ width: "18rem" }}>
+							<Card.Img variant="top" src={`${img}`} />
+							<Card.Body>
+								<Card.Title>{title}</Card.Title>
+								<Card.Text>{company}</Card.Text>
+								<Button variant="primary" href={`${link}`}>
+									Click and Check it out
+								</Button>
+							</Card.Body>
+						</Card>
+					</li>
+				);
+			})}
 		</>
 	);
 };
