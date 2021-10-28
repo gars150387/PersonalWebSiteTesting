8a12392bfe700f5c02e4f053da78b3b72c5c2379
gars150387 <gar.santeliz@gmail.com>
2021-10-28 15:54:27 +0000

    more certifications added

--- src/js/component/CardComp.js
+++ src/js/component/CardComp.js
@@ -6,12 +6,12 @@ import "../../img/BuildAGoogleMapAppWithJavascript.jpg";
 // import "../../img/DevelopingACriticalMindset.jpg";
 import "../../img/FirebaeEssentialTraining.jpg";
 import "../../img/JavascriptCodeChallenges.jpg";
-import "../../img/Node.jsEssentialTraining.jpg";
+import "../../img/NodejsEssentialTraining.jpg";
 // import "../../img/React Creating and Hosting a Full Stack Site.PNG";
 import "../../img/React.jsEssentialTraining.jpg";
 import "../../img/ReactBuildingAnInterface.jpg";
 import "../../img/ReactComponentsContextAndAccessibility.jpg";
-import "../../img/ReactDesignPatternsCertification.jpg";
+import "../../img/ReactDesignPatterns.jpg";
 import "../../img/ReactEcosystems.jpg";
 import "../../img/ReactHooks.jpg";
 // import "../../img/ReactDesignPatternscertification.jpg";
@@ -24,11 +24,11 @@ export const CardComp = props => {
 					<ul key={index}>
 						<li className="m-auto">
 							<Card md={6} className="m-auto" style={{ width: "18rem" }}>
-								<Card.Img variant="top" src={`./${props.img}.jpg`} />
+								<Card.Img variant="top" src={`./${this.img}.jpg`} />
 								<Card.Body>
-									<Card.Title>{props.title}</Card.Title>
-									<Card.Text>{props.company}</Card.Text>
-									<Button variant="primary" href={props.link}>
+									<Card.Title>{this.title}</Card.Title>
+									<Card.Text>{this.company}</Card.Text>
+									<Button variant="primary" href={this.link}>
 										Click and Check it out
 									</Button>
 								</Card.Body>
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>Deserved Rights by G.A.R.S <i className=" fab fa-react"/></p>
+	<footer className="footer mt-auto py-3 text-center bg-dark">
+		<p className="text-white">
+			Copy rights by G.A.R.S <i className=" fab fa-react" />
+		</p>
 	</footer>
 );
