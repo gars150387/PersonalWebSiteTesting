24c2bdd031e7460e55430d3c7f0508523dd201b9
gars150387 <gar.santeliz@gmail.com>
2021-11-01 19:22:29 +0000

    breadcrump removed

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -6,9 +6,14 @@ export const AboutMe = () => {
 	return (
 		<>
 			<Card style={{ width: "75%", margin: "auto" }}>
-				<Card.Img className="justify-content-end" style={{ width: "35%" }} variant="top" src="rigo-baby.jpg" />
+				<Card.Img
+					className="justify-content-end"
+					style={{ width: "100%", height: "50%" }}
+					variant="top"
+					src="rigo-baby.jpg"
+				/>
 				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
+					<Card.Title>This is Me!</Card.Title>
 					<Card.Text>Here is the part where I tell you my story!</Card.Text>
 				</Card.Body>
 			</Card>
