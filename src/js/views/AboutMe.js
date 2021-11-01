37b9ef1d9fa9204a54cccf41e70aed465a6ce168
gars150387 <gar.santeliz@gmail.com>
2021-11-01 19:58:47 +0000

    setting up position of cards components

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -1,11 +1,59 @@
 import React from "react";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const AboutMe = () => {
 	return (
 		<div className="">
-			<row>
+			<Row xs={1} md={2}>
+				<Col>
+					<Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
+						<Card.Img
+							className="justify-content-end"
+							style={{ width: "55%", height: "50%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/>
+						<Card.Body>
+							<Card.Title>This is Me!</Card.Title>
+							<Card.Text>Here is the part where I tell you my story!</Card.Text>
+						</Card.Body>
+						{/* <p>This is part of my story</p> */}
+					</Card>
+				</Col>
+				<Col>
+					<Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
+						<Card.Img
+							className="justify-content-end"
+							style={{ width: "55%", height: "50%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/>
+						<Card.Body>
+							<Card.Title>This is Me!</Card.Title>
+							<Card.Text>Here is the part where I tell you my story!</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+			<Row>
+				<Col xs={6} md={4} />
+				<Col xs={12} md={8}>
+					<Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
+						<Card.Img
+							className="justify-content-end"
+							style={{ width: "55%", height: "50%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/>
+						<Card.Body>
+							<Card.Title>This is Me!</Card.Title>
+							<Card.Text>Here is the part where I tell you my story!</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+			<Row>
 				<Col xs={12} md={8}>
 					<Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
 						<Card.Img
@@ -21,41 +69,7 @@ export const AboutMe = () => {
 					</Card>
 				</Col>
 				<Col xs={6} md={4} />
-			</row>
-			<row>
-				<Col xs={6} md={4} />
-				<Col xs={12} md={8}>
-					<Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
-						<Card.Img
-							className="justify-content-end"
-							style={{ width: "55%", height: "50%" }}
-							variant="top"
-							src="rigo-baby.jpg"
-						/>
-						<Card.Body>
-							<Card.Title>This is Me!</Card.Title>
-							<Card.Text>Here is the part where I tell you my story!</Card.Text>
-						</Card.Body>
-					</Card>
-				</Col>
-			</row>
-			<row>
-				<Col xs={12} md={8}>
-					<Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
-						<Card.Img
-							className="justify-content-end"
-							style={{ width: "55%", height: "50%" }}
-							variant="top"
-							src="rigo-baby.jpg"
-						/>
-						<Card.Body>
-							<Card.Title>This is Me!</Card.Title>
-							<Card.Text>Here is the part where I tell you my story!</Card.Text>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col xs={6} md={4} />
-			</row>
+			</Row>
 		</div>
 	);
 };
