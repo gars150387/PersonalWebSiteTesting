0b06cf58261d29dba4f8850accb6120f87966743
gars150387 <gar.santeliz@gmail.com>
2021-11-01 19:20:52 +0000

    setting up card body

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export const AboutMe = () => {
 	return (
 		<>
-			<Card style={{ width: "75%" }}>
-				<Card.Img variant="bottom" src="rigo-baby.jpg" />
+			<Card style={{ width: "75%", margin: "auto" }}>
+				<Card.Img className="justify-content-end" style={{ width: "35%" }} variant="top" src="rigo-baby.jpg" />
 				<Card.Body>
 					<Card.Title>Card Title</Card.Title>
 					<Card.Text>Here is the part where I tell you my story!</Card.Text>
