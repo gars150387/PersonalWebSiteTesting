339ecdb18a62583d46672d21046f05e07d912ca9
gars150387 <gar.santeliz@gmail.com>
2021-11-01 23:21:41 +0000

    copyrights reserved statement

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center bg-dark">
 		<p className="text-white">
-			Reserved copy rights by G.A.R.S <i className=" fab fa-react" />
+			Copyrights reserved by G.A.R.S <i className=" fab fa-react" />
 		</p>
 	</footer>
 );
