a85895992e1ab64a84a69f705c75bc4e32f33654
gars150387 <gar.santeliz@gmail.com>
2021-11-03 07:49:12 +0000

    add video to project component

--- src/js/views/Projects.js
+++ src/js/views/Projects.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
+import VideoPlayer from "react-video-js-player";
+import "../../video/PersonalWebsiteInProgress.mp4";
 
 export const Projects = () => {
+	const VideoSrc = PersonalWebsiteInProgress;
+
 	return (
 		<>
 			<Row className="m-auto">
@@ -24,7 +28,12 @@ export const Projects = () => {
 								content.
 							</Card.Text>
 						</Card.Body>
-						<ReactPlayer controls url="https://youtu.be/-QgJgZCJvo4" />
+						<VideoPlayer src={VideoSrc} width="720px" height="420px" playBackRates={[0.5, 1, 3.85, 16]} />
+						{/* <ReactPlayer
+							controls
+							url="/workspace/PersonalWebSiteTesting/src/video/Projects.js - PersonalWebSiteTesting - Gitpod Code.mp4"
+							src=""
+						/> */}
 					</Card>
 				</Col>
 			</Row>
