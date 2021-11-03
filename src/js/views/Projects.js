9d6e6da5d8bed2ab7351b91040fbd05b638c60a1
gars150387 <gar.santeliz@gmail.com>
2021-11-03 08:45:55 +0000

    add hyperlink to connect video on project component

--- src/js/views/Projects.js
+++ src/js/views/Projects.js
@@ -1,40 +1,44 @@
 import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
-// import ReactPlayer from "react-player";
-import VideoPlayer from "react-video-js-player";
-import "../../video/PersonalWebsiteInProgress.mp4";
+import ReactPlayer from "react-player";
+// import VideoPlayer from "react-video-js-player";
+// import "../../video/PersonalWebsiteInProgress.mp4";
 
 export const Projects = () => {
-	const VideoSrc = PersonalWebsiteInProgress;
+	// const VideoSrc = PersonalWebsiteInProgress;
 
 	return (
 		<>
 			<Row className="m-auto">
 				<Col className="m-auto">
-					<Card>
-						<Card.Img style={{ width: "35px", height: "35px" }} variant="top" src="rigo-baby.jpg" rounded />
-						<Card.Body>
-							<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of the cards
-								content.
-							</Card.Text>
-						</Card.Body>
-					</Card>
+					<Card.Img style={{ width: "35px", height: "35px" }} variant="top" src="rigo-baby.jpg" rounded />
+					<Card.Body>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the cards
+							content.
+						</Card.Text>
+					</Card.Body>{" "}
 					<br />
-					<Card>
-						<Card.Body>
-							<Card.Text>
-								Some quick example text to build on the card title and make up the bulk of the cards
-								content.
-							</Card.Text>
-						</Card.Body>
-						<VideoPlayer src={VideoSrc} width="720px" height="420px" playBackRates={[0.5, 1, 3.85, 16]} />
-						{/* <ReactPlayer
-							controls
-							url="/workspace/PersonalWebSiteTesting/src/video/Projects.js - PersonalWebSiteTesting - Gitpod Code.mp4"
-							src=""
-						/> */}
-					</Card>
+					<Card.Body>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of the cards
+							content.
+						</Card.Text>
+					</Card.Body>
+					{/* <VideoPlayer src={VideoSrc} width="720px" height="420px" playBackRates={[0.5, 1, 3.85, 16]} /> */}
+					<ReactPlayer
+						autoPlay={true}
+						className="d-flex m-auto rouned border-solid border-transparency"
+						controls
+						url="https://cdn.loom.com/sessions/thumbnails/45c055eff97c49e6aa79d7b2e9d2c4ff-00001.mp4#t=0.1"
+						src=""
+					/>
+					<h5 className="text-center">
+						To watch full video of the progression of my website,{" "}
+						<button className="bg-success rounded" href="https://www.loom.com/my-videos">
+							click here
+						</button>
+					</h5>
 				</Col>
 			</Row>
 		</>
