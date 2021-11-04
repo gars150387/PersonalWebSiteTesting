c1fffb3110914234944c6bfbdeb6e8da135b78dd
gars150387 <gar.santeliz@gmail.com>
2021-11-04 18:41:15 +0000

    reminder to add animation

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -23,7 +23,7 @@ export const AboutMe = () => {
 							src="rigo-baby.jpg"
 						/> */}
 					<Card.Body className="text-start">
-						<Card.Title>This is Me!</Card.Title>
+						<Card.Title>Add Motion to provide animetion to this paragraph</Card.Title>
 						<Card.Text>Here is the part where I tell you my story!</Card.Text>
 					</Card.Body>
 					{/* </Card> */}
