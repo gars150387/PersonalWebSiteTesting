6e54299b0fd84b00188cfdd5befbe4d0e9c3aacb
gars150387 <gar.santeliz@gmail.com>
2021-11-07 14:44:32 +0000

    structure drag component

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -1,11 +1,77 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Drag } from "../component/Drag";
+import { Board } from "../component/Board";
 
 export const AboutMe = () => {
 	return (
-		<div className="">
-			<Row>
+		<div className="flexbox">
+			<Board id="board-1" className="baord">
+			<Drag id="drag-1" className="drag" draggable="true">
+			<Col className="text-end">
+					<Card.Img
+						// className="justify-content-end"
+						style={{ width: "60%", height: "60%" }}
+						variant="top"
+						src="rigo-baby.jpg"
+					/>
+				</Col>
+				<Drag>
+				<Col>
+					{/* <Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
+						<Card.Img
+							className="justify-content-end"
+							style={{ width: "55%", height: "50%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/> */}
+					<Card.Body className="text-start">
+						<Card.Title>Add Motion to provide animetion to this paragraph</Card.Title>
+						<Card.Text>Here is the part where I tell you my story!</Card.Text>
+					</Card.Body>
+					{/* </Card> */}
+				</Col>
+				</Drag>
+				<Drag>
+				<Col>
+					{" "}
+					<Card.Body className="text-end">
+						<Card.Title>This is Me!</Card.Title>
+						<Card.Text>Here is the part where I tell you my story!</Card.Text>
+					</Card.Body>
+				</Col>
+				</Drag>
+				<Drag>
+				<Col>
+					<Card.Img
+						// className="justify-content-end"
+						style={{ width: "60%", height: "60%" }}
+						variant="top"
+						src="rigo-baby.jpg"
+					/>
+				</Col>
+				</Drag>
+				<Col className="text-end">
+					<Card.Img
+						// className="justify-content-end"
+						style={{ width: "60%", height: "60%" }}
+						variant="top"
+						src="rigo-baby.jpg"
+					/>
+				</Col>
+				<Col>
+					<Card.Body className="text-start">
+						<Card.Title>This is Me!</Card.Title>
+						<Card.Text>Here is the part where I tell you my story!</Card.Text>
+					</Card.Body>
+				</Col>
+
+			</Drag>
+			</Board>
+			<Board id="board-2" className="board">
+				<Drag id="drag-2" className="drag" draggable="true">
+				<Row>
 				<Col className="text-end">
 					<Card.Img
 						// className="justify-content-end"
@@ -62,6 +128,9 @@ export const AboutMe = () => {
 					</Card.Body>
 				</Col>
 			</Row>
+
+				</Drag>
+			</Board>
 		</div>
 	);
 };
