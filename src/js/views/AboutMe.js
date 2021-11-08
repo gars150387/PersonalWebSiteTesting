df50aaccec4375b739569c5e698ec2becf73265d
gars150387 <gar.santeliz@gmail.com>
2021-11-08 13:18:44 +0000

    declare props

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -8,71 +8,71 @@ export const AboutMe = () => {
 	return (
 		<div className="flexbox">
 			<Board id="board-1" className="baord">
-			<Drag id="drag-1" className="drag" draggable="true">
-			<Col className="text-end">
-					<Card.Img
-						// className="justify-content-end"
-						style={{ width: "60%", height: "60%" }}
-						variant="top"
-						src="rigo-baby.jpg"
-					/>
-				</Col>
+				<Drag id="drag-1" className="drag" draggable="true">
+					<Col className="text-end">
+						<Card.Img
+							// className="justify-content-end"
+							style={{ width: "60%", height: "60%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/>
+					</Col>
 				</Drag>
 				<Drag id="drag-3" className="drag" draggable="true">
-				<Col>
-					{" "}
-					<Card.Body className="text-end">
-						<Card.Title>This is Me!</Card.Title>
-						<Card.Text>Here is the part where I tell you my story!</Card.Text>
-					</Card.Body>
-				</Col>
+					<Col>
+						{" "}
+						<Card.Body className="text-end">
+							<Card.Title>This is Me!</Card.Title>
+							<Card.Text>Here is the part where I tell you my story!</Card.Text>
+						</Card.Body>
+					</Col>
 				</Drag>
 				<Drag id="drag-5" className="drag" draggable="true">
-				<Col className="text-end">
-					<Card.Img
-						// className="justify-content-end"
-						style={{ width: "60%", height: "60%" }}
-						variant="top"
-						src="rigo-baby.jpg"
-					/>
-				</Col>
+					<Col className="text-end">
+						<Card.Img
+							// className="justify-content-end"
+							style={{ width: "60%", height: "60%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/>
+					</Col>
 				</Drag>
 			</Board>
 			<Board id="board-2" className="board">
-			<Drag id="drag-2" className="drag" draggable="true">
-				<Col>
-					{/* <Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
+				<Drag id="drag-2" className="drag" draggable="true">
+					<Col>
+						{/* <Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
 						<Card.Img
 							className="justify-content-end"
 							style={{ width: "55%", height: "50%" }}
 							variant="top"
 							src="rigo-baby.jpg"
 						/> */}
-					<Card.Body className="text-start">
-						<Card.Title>Add Motion to provide animetion to this paragraph</Card.Title>
-						<Card.Text>Here is the part where I tell you my story!</Card.Text>
-					</Card.Body>
-					{/* </Card> */}
-				</Col>
+						<Card.Body className="text-start">
+							<Card.Title>Add Motion to provide animetion to this paragraph</Card.Title>
+							<Card.Text>Here is the part where I tell you my story!</Card.Text>
+						</Card.Body>
+						{/* </Card> */}
+					</Col>
 				</Drag>
 				<Drag id="drag-4" className="drag" draggable="true">
-				<Col>
-					<Card.Img
-						// className="justify-content-end"
-						style={{ width: "60%", height: "60%" }}
-						variant="top"
-						src="rigo-baby.jpg"
-					/>
-				</Col>
+					<Col>
+						<Card.Img
+							// className="justify-content-end"
+							style={{ width: "60%", height: "60%" }}
+							variant="top"
+							src="rigo-baby.jpg"
+						/>
+					</Col>
 				</Drag>
 				<Drag id="drag-6" className="drag" draggable="true">
-				<Col>
-					<Card.Body className="text-start">
-						<Card.Title>This is Me!</Card.Title>
-						<Card.Text>Here is the part where I tell you my story!</Card.Text>
-					</Card.Body>
-				</Col>
-			</Drag>
+					<Col>
+						<Card.Body className="text-start">
+							<Card.Title>This is Me!</Card.Title>
+							<Card.Text>Here is the part where I tell you my story!</Card.Text>
+						</Card.Body>
+					</Col>
+				</Drag>
 			</Board>
 		</div>
 	);
