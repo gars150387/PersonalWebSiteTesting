870b86bf6565db4a5830888493219ec52108f72c
gars150387 <gar.santeliz@gmail.com>
2021-11-10 20:02:53 +0000

    remove drag option from About Me component

--- src/js/component/Board.js
+++ src/js/component/Board.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { PropTypes } from "prop-types";
+// import React from "react";
+// import { PropTypes } from "prop-types";
 
-export const Board = props => {
-	const drop = e => {
-		e.preventDefault();
-		const card_id = e.dataTransfer("card_id");
-		const card = document.getElementById(card_id);
-		card.style.display = "block";
+// export const Board = props => {
+// 	const drop = e => {
+// 		e.preventDefault();
+// 		const card_id = e.dataTransfer("card_id");
+// 		const card = document.getElementById(card_id);
+// 		card.style.display = "block";
 
-		e.target.appendChild(card);
-	};
-	const dragOver = e => {
-		e.preventDefault();
-	};
+// 		e.target.appendChild(card);
+// 	};
+// 	const dragOver = e => {
+// 		e.preventDefault();
+// 	};
 
-	return (
-		<div id={props.id} className={props.className} onDrop={drop} onDragOver={dragOver}>
-			{props.children}
-		</div>
-	);
-};
-Board.prototype = {
-	id: PropTypes.number,
-	className: PropTypes.string,
-	draggable: PropTypes.boolean,
-	children: PropTypes.element.isRequired
-};
+// 	return (
+// 		<div id={props.id} className={this.props.className} onDrop={drop} onDragOver={dragOver}>
+// 			{props.children}
+// 		</div>
+// 	);
+// };
+// Board.propType = {
+// 	id: PropTypes.number,
+// 	className: PropTypes.string,
+// 	draggable: PropTypes.boolean,
+// 	children: PropTypes.element.isRequired
+// };
--- src/js/component/Drag.js
+++ src/js/component/Drag.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { PropTypes } from "prop-types";
+// import React from "react";
+// import { PropTypes } from "prop-types";
 
-export const Drag = props => {
-	const dragStart = e => {
-		const target = e.target;
-		e.dataTransfer.setData("card_id", target.id);
+// export const Drag = props => {
+// 	const dragStart = e => {
+// 		const target = e.target;
+// 		e.dataTransfer.setData("card_id", target.id);
 
-		setTimeout(() => {
-			target.style.display = "none";
-		}, 0);
-	};
-	const dragOver = e => {
-		e.stopPropagation();
-	};
-	return (
-		<div
-			id={props.id}
-			className={props.className}
-			draggable={props.draggable}
-			onDragStart={dragStart}
-			onDragOver={dragOver}>
-			{props.childern}
-		</div>
-	);
-};
-Drag.prototype = {
-	id: PropTypes.number,
-	className: PropTypes.string,
-	draggable: PropTypes.boolean,
-	children: PropTypes.element.isRequired
-};
+// 		setTimeout(() => {
+// 			target.style.display = "none";
+// 		}, 0);
+// 	};
+// 	const dragOver = e => {
+// 		e.stopPropagation();
+// 	};
+// 	return (
+// 		<div
+// 			id={props.id}
+// 			className={props.className}
+// 			draggable={props.draggable}
+// 			onDragStart={dragStart}
+// 			onDragOver={dragOver}>
+// 			{props.childern}
+// 		</div>
+// 	);
+// };
+// Drag.prototype = {
+// 	id: PropTypes.number,
+// 	className: PropTypes.string,
+// 	draggable: PropTypes.boolean,
+// 	children: PropTypes.element.isRequired
+// };
