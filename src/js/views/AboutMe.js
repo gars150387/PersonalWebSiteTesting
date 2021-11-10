abd4fbe4b54e6322c9e6a4bc98019a2b359e35b6
gars150387 <gar.santeliz@gmail.com>
2021-11-10 20:06:02 +0000

    set rows

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -1,63 +1,69 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Drag } from "../component/Drag";
-import { Board } from "../component/Board";
+// import { Drag } from "../component/Drag";
+// import { Board } from "../component/Board";
 
 export const AboutMe = () => {
 	return (
 		<div className="flexbox">
-			<Col className="text-end">
-				<Card.Img
-					// className="justify-content-end"
-					style={{ width: "60%", height: "60%" }}
-					variant="top"
-					src="rigo-baby.jpg"
-				/>
-			</Col>
-			<Col>
-				{" "}
-				<Card.Body className="text-end">
-					<Card.Title>This is Me!</Card.Title>
-					<Card.Text>Here is the part where I tell you my story!</Card.Text>
-				</Card.Body>
-			</Col>
-			<Col className="text-end">
-				<Card.Img
-					// className="justify-content-end"
-					style={{ width: "60%", height: "60%" }}
-					variant="top"
-					src="rigo-baby.jpg"
-				/>
-			</Col>
-			<Col>
-				{/* <Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
+			<Row>
+				<Col className="text-end">
+					<Card.Img
+						// className="justify-content-end"
+						style={{ width: "60%", height: "60%" }}
+						variant="top"
+						src="rigo-baby.jpg"
+					/>
+				</Col>
+				<Col>
+					{" "}
+					<Card.Body className="text-end">
+						<Card.Title>This is Me!</Card.Title>
+						<Card.Text>Here is the part where I tell you my story!</Card.Text>
+					</Card.Body>
+				</Col>
+			</Row>
+			<Row>
+				<Col className="text-end">
+					<Card.Img
+						// className="justify-content-end"
+						style={{ width: "60%", height: "60%" }}
+						variant="top"
+						src="rigo-baby.jpg"
+					/>
+				</Col>
+				<Col>
+					{/* <Card className="justify-content-start" style={{ width: "60%", margin: "auto" }}>
 						<Card.Img
 							className="justify-content-end"
 							style={{ width: "55%", height: "50%" }}
 							variant="top"
 							src="rigo-baby.jpg"
 						/> */}
-				<Card.Body className="text-start">
-					<Card.Title>Add Motion to provide animetion to this paragraph</Card.Title>
-					<Card.Text>Here is the part where I tell you my story!</Card.Text>
-				</Card.Body>
-				{/* </Card> */}
-			</Col>
-			<Col>
-				<Card.Img
-					// className="justify-content-end"
-					style={{ width: "60%", height: "60%" }}
-					variant="top"
-					src="rigo-baby.jpg"
-				/>
-			</Col>
-			<Col>
-				<Card.Body className="text-start">
-					<Card.Title>This is Me!</Card.Title>
-					<Card.Text>Here is the part where I tell you my story!</Card.Text>
-				</Card.Body>
-			</Col>
+					<Card.Body className="text-start">
+						<Card.Title>Add Motion to provide animetion to this paragraph</Card.Title>
+						<Card.Text>Here is the part where I tell you my story!</Card.Text>
+					</Card.Body>
+					{/* </Card> */}
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<Card.Img
+						// className="justify-content-end"
+						style={{ width: "60%", height: "60%" }}
+						variant="top"
+						src="rigo-baby.jpg"
+					/>
+				</Col>
+				<Col>
+					<Card.Body className="text-start">
+						<Card.Title>This is Me!</Card.Title>
+						<Card.Text>Here is the part where I tell you my story!</Card.Text>
+					</Card.Body>
+				</Col>
+			</Row>
 		</div>
 	);
 };
